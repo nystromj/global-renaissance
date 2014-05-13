c1fa362bca03a60a84ceb7f6832b9af6201cced4
Graeme Boy <graemeboy@gmail.com>
2014-05-12 20:49:06 -0500

    First commit from Graeme

--- website/app/routes/index.js
+++ website/app/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Homepage' });
+});
+
+/* GET home page. */
+router.get('/map', function(req, res) {
+  res.render('map/map', { title: 'Map' });
 });
 
 module.exports = router;
