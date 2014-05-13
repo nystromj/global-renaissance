c3dee5ed6a847d879c715a1887ffe1fd945dbfd6
Graeme Boy <graemeboy@gmail.com>
2014-05-12 21:22:38 -0500

    Fix everything else

--- app.js
+++ app.js
@@ -11,9 +11,14 @@ require("./app/config/express")(app, config);
 app.set('port', 3000);
 
 // Server
-http.createServer(app).listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
-}); // createServer
+//http.createServer(app).listen(app.get('port'), function () {
+//  console.log('Express server listening on port ' + app.get('port'));
+//}); // createServer
+
+app.set('port', process.env.PORT || 3000);
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
 
 // Exports
 
